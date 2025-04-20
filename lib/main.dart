75cbd4454453ddac0956e75745ba1990a8c33214
koren <nowtoknow@gmail.com>
2025-04-21 03:29:17 +0800

    aa

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,23 @@
-import 'package:fitness_dashboard_ui/const/constant.dart';
-import 'package:fitness_dashboard_ui/screens/main_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:fitness_dashboard_ui/screens/main_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'KOREN POGI',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: backgroundColor,
-        brightness: Brightness.dark,
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: "AIzaSyCUtV7uuQdRm32ylRATJczcs5YqxmoCRuM",
+        authDomain: "capstone-forked.firebaseapp.com",
+        projectId: "capstone-forked",
+        storageBucket: "capstone-forked.firebasestorage.app",
+        messagingSenderId: "843926843316",
+        appId: "1:843926843316:web:00aa960c3d17a8a55b4ce1",
+        databaseURL: "https://capstone-forked-default-rtdb.asia-southeast1.firebasedatabase.app", // Add this line
       ),
-      home: const MainScreen(),
     );
+  } catch (e) {
+    print("Firebase initialization error: $e");
   }
+  runApp(const MainScreen());
 }
--- lib/widgets/dashboard_widget.dart
+++ lib/widgets/dashboard_widget.dart
@@ -2,7 +2,6 @@ import 'package:fitness_dashboard_ui/util/responsive.dart';
 import 'package:fitness_dashboard_ui/widgets/activity_details_card.dart';
 import 'package:fitness_dashboard_ui/widgets/bar_graph_widget.dart';
 import 'package:fitness_dashboard_ui/widgets/header_widget.dart';
-import 'package:fitness_dashboard_ui/widgets/line_chart_card.dart';
 import 'package:fitness_dashboard_ui/widgets/summary_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -16,15 +15,15 @@ class DashboardWidget extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 18),
         child: Column(
           children: [
-            const SizedBox(height: 18),
+            const SizedBox(height: 10),
             const HeaderWidget(),
             const SizedBox(height: 18),
             Center(child: const ActivityDetailsCard()),
             const SizedBox(height: 18),
-            const LineChartCard(),
-            const SizedBox(height: 18),
+            // const LineChartCard(),
+            // const SizedBox(height: 18),
             const BarGraphCard(),
-            const SizedBox(height: 18),
+            const SizedBox(height: 50),
             if (Responsive.isTablet(context)) const SummaryWidget(),
           ],
         ),
